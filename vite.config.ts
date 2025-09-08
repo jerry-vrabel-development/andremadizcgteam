2e65db45dbeee36cb1217010b6174aa2ab51d2fa
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2025-09-08 14:41:13 -0500

    PWA servicework and manifest added

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: "auto",
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'android-chrome-192x192.png'],
+      manifest: {
+        name: 'Andre Madiz Team',
+        short_name: 'Andre Madiz Team',
+        description: 'Come Train With The Best.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallback: "/",
+      },
+    }),
   ],
+  base: '/andremadizcgteam/',
 })

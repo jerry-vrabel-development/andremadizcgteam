6148d53ed337c749b504d99527dbac8208d7d89f
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2025-09-09 10:57:28 -0500

    Photos component work *wip

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-b49cbce6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k2fibl2gsf8"
+    "revision": "0.01bceqes9c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

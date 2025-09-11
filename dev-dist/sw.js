920fb36e5fd77e57bd433a2cdc2ce9a072b84fb5
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2025-09-11 10:35:13 -0500

    Home component styling

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-b49cbce6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3cfqj9fsq1g"
+    "revision": "0.f1kbiurideo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
